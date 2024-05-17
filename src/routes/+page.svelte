a40c34a79fe77f4432044d049dd1e8a3b570aac7
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-05-17 12:56:04 +0530

    install svelte and setup normalize css

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style lang="scss">
+    @use '../style/functions';
+  h1 {
+    color: red;
+    font-size: functions.toRem(36);
+  }
+</style>
